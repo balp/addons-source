c7cfc96bae1d6214f7cf9d630b57b208e001421d
Anders Arnholm <anders@arnholm.se>
2020-02-02 15:43:26 +0100

    Support place types of Sweden, relations with dates

--- WikidataPlaces/WikidataPlaces.py
+++ WikidataPlaces/WikidataPlaces.py
@@ -5,10 +5,42 @@ from qwikidata.datavalue import GlobeCoordinate
 
 from gramps.gen.const import COLON, GRAMPS_LOCALE as glocale
 from gramps.gen.db import DbTxn
-from gramps.gen.lib import Place, PlaceName, PlaceType, Url, PlaceRef
+from gramps.gen.lib import Place, PlaceName, PlaceType, Url, PlaceRef, Date
 from gramps.gen.plug import Gramplet
 from qwikidata.entity import WikidataItem
 from qwikidata.linked_data_interface import get_entity_dict_from_api
+
+INSTANCE_FORMER_COUNTY_OF_SWEDEN = 'Q64624092'
+
+PROPERTY_LOCATED = 'P276'
+PROPERTY_LOCATED_IN_ADM = 'P131'
+PROPERTY_COORDINATE_LOCATION = 'P625'
+PROPERTY_INSTANCE_OF = 'P31'
+
+ITEM_BUILDING = 'Q41176'
+ITEM_FARM = 'Q131596'
+ITEM_HAMLET = 'Q5084'
+ITEM_VILLAGE = 'Q532'
+ITEM_LARGE_VILLAGE = 'Q26714626'
+ITEM_TOWN = 'Q3957'
+ITEM_BOROUGH = 'Q5195043'
+ITEM_DISTRICT = 'Q149621'
+ITEM_NEIGHBORHOOD = 'Q123705'
+ITEM_ADM_REGION = 'Q3455524'
+ITEM_COUNTY_OF_SWEDEN = 'Q200547'
+ITEM_COUNTY = 'Q28575'
+ITEM_PROVINCE_OF_SWEDEN = 'Q193556'
+ITEM_PROVINCE = 'Q34876'
+ITEM_FEDERAL_STATE = 'Q107390'
+ITEM_STATE_OF_US = 'Q35657'
+ITEM_COUNTRY = 'Q6256'
+ITEM_SOVEREIGN_STATE = 'Q3624078'
+ITEM_MUNICIPALITY = 'Q15284'
+ITEM_MUNICIPALITY_OF_SWEDEN = 'Q127448'
+ITEM_PARISH = 'Q102496'
+ITEM_SOCKEN = 'Q1523821'
+ITEM_ISLAND = 'Q23442'
+
 
 _ = glocale.translation.sgettext
 
@@ -45,26 +77,93 @@ def get_place_from_wikidata(entity_id):
             wikipedia_url.set_type('Wikipedia entry')
             wikipedia_url.set_description(f'Wikipedia {link["title"]}:{link["site"]}')
             place.add_url(wikipedia_url)
+
     # Instance of -> PlaceType
-    if 'P31' in claims_groups:
-        for claim in claims_groups['P31']:
-            if 'Q102496' == claim.mainsnak.datavalue.value['id']:
+    if PROPERTY_INSTANCE_OF in claims_groups:
+        for claim in claims_groups[PROPERTY_INSTANCE_OF]:
+            instance_of = claim.mainsnak.datavalue.value['id']
+            if ITEM_PARISH == instance_of:
                 place.set_type(PlaceType.PARISH)
-            if 'Q1523821' == claim.mainsnak.datavalue.value['id']:
+            elif ITEM_SOCKEN == instance_of:
                 place.set_type(PlaceType.PARISH)
-            if 'Q23442' == claim.mainsnak.datavalue.value['id']:
+            elif ITEM_ISLAND == instance_of:
                 place.set_type(PlaceType.UNKNOWN)  # No islands in Gramps
-            if 'Q127448' == claim.mainsnak.datavalue.value['id']:
-                place.set_type(PlaceType.MUNICIPALITY)  # municipality of Sweden
-    if 'P625' in claims_groups:
-        for claim in claims_groups['P625']:
+            elif ITEM_MUNICIPALITY_OF_SWEDEN == instance_of:
+                place.set_type(PlaceType.MUNICIPALITY)
+            elif ITEM_MUNICIPALITY == instance_of:
+                place.set_type(PlaceType.MUNICIPALITY)
+            elif ITEM_COUNTRY == instance_of:
+                place.set_type(PlaceType.COUNTRY)
+            elif ITEM_SOVEREIGN_STATE == instance_of:
+                place.set_type(PlaceType.COUNTRY)
+            elif ITEM_STATE_OF_US == instance_of:
+                place.set_type(PlaceType.STATE)
+            elif ITEM_FEDERAL_STATE == instance_of:
+                place.set_type(PlaceType.STATE)
+            elif ITEM_COUNTY == instance_of:
+                place.set_type(PlaceType.COUNTY)
+            elif ITEM_COUNTY_OF_SWEDEN == instance_of:
+                place.set_type(PlaceType.COUNTY)
+            elif INSTANCE_FORMER_COUNTY_OF_SWEDEN == instance_of:
+                place.set_type(PlaceType.COUNTY)
+            elif ITEM_PROVINCE_OF_SWEDEN == instance_of:
+                place.set_type(PlaceType.PROVINCE)
+            elif ITEM_PROVINCE == instance_of:
+                place.set_type(PlaceType.PROVINCE)
+            elif ITEM_ADM_REGION == instance_of:
+                place.set_type(PlaceType.REGION)
+            elif ITEM_NEIGHBORHOOD == instance_of:
+                place.set_type(PlaceType.NEIGHBORHOOD)
+            elif ITEM_DISTRICT == instance_of:
+                place.set_type(PlaceType.DISTRICT)
+            elif ITEM_BOROUGH == instance_of:
+                place.set_type(PlaceType.BOROUGH)
+            elif ITEM_TOWN == instance_of:
+                place.set_type(PlaceType.TOWN)
+            elif ITEM_LARGE_VILLAGE == instance_of:
+                place.set_type(PlaceType.VILLAGE)
+            elif ITEM_VILLAGE == instance_of:
+                place.set_type(PlaceType.VILLAGE)
+            elif ITEM_HAMLET == instance_of:
+                place.set_type(PlaceType.HAMLET)
+            elif ITEM_FARM == instance_of:
+                place.set_type(PlaceType.FARM)
+            elif ITEM_BUILDING == instance_of:
+                place.set_type(PlaceType.BUILDING)
+
+    if PROPERTY_COORDINATE_LOCATION in claims_groups:
+        for claim in claims_groups[PROPERTY_COORDINATE_LOCATION]:
             datavalue: GlobeCoordinate = claim.mainsnak.datavalue
             place.set_latitude(str(datavalue.value['latitude']))
             place.set_longitude(str(datavalue.value['longitude']))
-    if 'P131' in claims_groups:
-        for claim in claims_groups['P131']:
+
+    if PROPERTY_LOCATED_IN_ADM in claims_groups:
+        for claim in claims_groups[PROPERTY_LOCATED_IN_ADM]:
             if claim.mainsnak.snak_datatype == 'wikibase-item':
-                parents.add(claim.mainsnak.datavalue.value["id"])
+                item_id = claim.mainsnak.datavalue.value["id"]
+                start = None
+                end = None
+                for qualifier in claim.qualifiers.values():
+                    for qual in qualifier:
+                        if qual.snak.property_id == 'P580':  # Start time
+                            start = qual.snak.datavalue
+                        if qual.snak.property_id == 'P582':  # End time
+                            end = qual.snak.datavalue
+                parents.add((item_id, start, end))
+
+    if PROPERTY_LOCATED in claims_groups:
+        for claim in claims_groups[PROPERTY_LOCATED]:
+            if claim.mainsnak.snak_datatype == 'wikibase-item':
+                item_id = claim.mainsnak.datavalue.value["id"]
+                start = None
+                end = None
+                for qualifier in claim.qualifiers.values():
+                    for qual in qualifier:
+                        if qual.snak.property_id == 'P580':  # Start time
+                            start = qual.snak.datavalue
+                        if qual.snak.property_id == 'P582':  # End time
+                            end = qual.snak.datavalue
+                parents.add((item_id, start, end))
 
 
     return place, parents
@@ -111,27 +210,72 @@ class WikidataPlacesGramplet(Gramplet):
         if len(entity_id):
             buffer.set_text(f"Adding entity {entity_id} from Wikidata.\n")
             todo = set()
-            todo.add(entity_id)
+            todo.add((entity_id, None, None))
             done = set()
             all_places = set()
             links_to_add = set()
 
             while len(todo) > 0:
-                current_entity = todo.pop()
-                buffer.set_text(f"Working on {current_entity}:\n")
+                current_entity, current_start, current_end = todo.pop()
+                buffer.insert(buffer.get_end_iter(), f"Working on {current_entity}:\n")
                 done.add(current_entity)
                 wikidata_place, parents = get_place_from_wikidata(current_entity)
-                for parent in parents:
+                for parent, start, end in parents:
                     if parent not in done:
-                        todo.add(parent)
-                    #  TODO Add parent refs even when child comes before
+                        todo.add((parent, start, end))
                     if self.dbstate.db.has_place_gramps_id(parent):
                         parent_ent = self.dbstate.db.get_place_from_gramps_id(parent)
                         parent_ref = PlaceRef()
                         parent_ref.set_reference_handle(parent_ent.get_handle())
+                        buffer.insert(buffer.get_end_iter(), f"start: {start} end: {end}\n")
+                        if start is not None:
+                            if end is not None:
+                                date = Date()
+                                start_dict = start.get_parsed_datetime_dict()
+                                end_dict = end.get_parsed_datetime_dict()
+                                DD1 = start_dict['day']
+                                MM1 = start_dict['month']
+                                YY1 = start_dict['year']
+                                slash1 = False
+                                DD2 = end_dict['day']
+                                MM2 = end_dict['month']
+                                YY2 = end_dict['year']
+                                slash2 = False
+                                date.set(quality=None,
+                                         modifier=Date.MOD_SPAN,
+                                         calendar=None,
+                                         value=(DD1, MM1, YY1, slash1, DD2, MM2, YY2, slash2),
+                                         text=None)
+                                parent_ref.set_date_object(date=date)
+                            else:
+                                date = Date()
+                                start_dict = start.get_parsed_datetime_dict()
+                                DD1 = start_dict['day']
+                                MM1 = start_dict['month']
+                                YY1 = start_dict['year']
+                                slash1 = False
+                                date.set(quality=None,
+                                         modifier=Date.MOD_AFTER,
+                                         calendar=None,
+                                         value=(DD1, MM1, YY1, slash1),
+                                         text=None)
+                                parent_ref.set_date_object(date=date)
+                        elif end is not None:
+                            date = Date()
+                            end_dict = end.get_parsed_datetime_dict()
+                            DD1 = end_dict['day']
+                            MM1 = end_dict['month']
+                            YY1 = end_dict['year']
+                            slash1 = False
+                            date.set(quality=None,
+                                     modifier=Date.MOD_BEFORE,
+                                     calendar=None,
+                                     value=(DD1, MM1, YY1, slash1),
+                                     text=None)
+                            parent_ref.set_date_object(date=date)
                         wikidata_place.add_placeref(parent_ref)
                     else:
-                        links_to_add.add((current_entity, parent))
+                        links_to_add.add((current_entity, parent, start, end))
 
                 if self.dbstate.db.has_place_gramps_id(current_entity):
                     place = self.dbstate.db.get_place_from_gramps_id(current_entity)
@@ -149,10 +293,57 @@ class WikidataPlacesGramplet(Gramplet):
                     self.dbstate.db.commit_place(place, trans)
 
             with DbTxn(_('Update parent references (enclosed by)'), self.dbstate.db) as trans:
-                for child, parent in links_to_add:
+                for child, parent, start, end in links_to_add:
                     parent_ent = self.dbstate.db.get_place_from_gramps_id(parent)
                     parent_ref = PlaceRef()
                     parent_ref.set_reference_handle(parent_ent.get_handle())
+
+                    if start is not None:
+                        if end is not None:
+                            date = Date()
+                            start_dict = start.get_parsed_datetime_dict()
+                            end_dict = end.get_parsed_datetime_dict()
+                            DD1 = start_dict['day']
+                            MM1 = start_dict['month']
+                            YY1 = start_dict['year']
+                            slash1 = False
+                            DD2 = end_dict['day']
+                            MM2 = end_dict['month']
+                            YY2 = end_dict['year']
+                            slash2 = False
+                            date.set(quality=None,
+                                     modifier=Date.MOD_SPAN,
+                                     calendar=None,
+                                     value=(DD1, MM1, YY1, slash1, DD2, MM2, YY2, slash2),
+                                     text=None)
+                            parent_ref.set_date_object(date=date)
+                        else:
+                            date = Date()
+                            start_dict = start.get_parsed_datetime_dict()
+                            DD1 = start_dict['day']
+                            MM1 = start_dict['month']
+                            YY1 = start_dict['year']
+                            slash1 = False
+                            date.set(quality=None,
+                                     modifier=Date.MOD_AFTER,
+                                     calendar=None,
+                                     value=(DD1, MM1, YY1, slash1),
+                                     text=None)
+                            parent_ref.set_date_object(date=date)
+                    elif end is not None:
+                        date = Date()
+                        end_dict = end.get_parsed_datetime_dict()
+                        DD1 = end_dict['day']
+                        MM1 = end_dict['month']
+                        YY1 = end_dict['year']
+                        slash1 = False
+                        date.set(quality=None,
+                                 modifier=Date.MOD_BEFORE,
+                                 calendar=None,
+                                 value=(DD1, MM1, YY1, slash1),
+                                 text=None)
+                        parent_ref.set_date_object(date=date)
+
                     child_ent = self.dbstate.db.get_place_from_gramps_id(child)
                     child_ent.add_placeref(parent_ref)
                     self.dbstate.db.commit_place(child_ent, trans)
